c993c28b07951d2fdb8474108bd7b1f540d94c97
Henry Zhang <hh.zhang520@gmail.com>
2025-06-06 00:42:23 +1000

    frontend skeleton

---
+++ frontend/src/global.d.ts
@@ -0,0 +1,10 @@
+declare module '*.svelte' {
+  import type { SvelteComponentTyped } from 'svelte';
+
+  // You can specify empty props/events/slots or keep them generic like this:
+  export default class SvelteComponent extends SvelteComponentTyped<
+    Record<string, unknown>, // props
+    Record<string, unknown>, // events
+    Record<string, unknown>  // slots
+  > {}
+}
